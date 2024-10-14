42e3b87413d285f202d7c94739448f581dc806dc
Armenak Darbinyan <armenak.darbinyan@gmail.com>
2024-10-14 18:56:53 +0300

    added Spring reactive application

---
+++ src/test/java/ru/hiws/sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hiws.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
